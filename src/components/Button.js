ffbbd66d3d320a5c60f5c51c40252ee34e0e0fe2
leurjim <shaleao@icloud.com>
2021-04-03 00:12:10 -0500

    cambiando tamaño de icono

--- src/components/Button.js
+++ src/components/Button.js
@@ -10,17 +10,17 @@ const Button = ({ to, children, secondary, ...restProps }) => {
     display: 'inline-flex',
     border: 'none',
     borderRadius: '4px',
-    // background: secondary ? rgba(colors.primary, 0.2) : colors.primary,
-    // color: secondary ? colors.primary : colors.white,
+    background: secondary ? rgba(colors.primary, 0.2) : colors.primary,
+    color: secondary ? colors.primary : colors.white,
     padding: '10px 15px',
     cursor: 'pointer',
     transition: 'all 150ms ease',
-    // '@media (hover: hover)': {
-    //   ':hover': {
-    //     color: colors.white,
-    //     background: darken(0.1, colors.primary),
-    //   },
-    // },
+    '@media (hover: hover)': {
+      ':hover': {
+        color: colors.white,
+        background: darken(0.1, colors.primary),
+      },
+    },
   })
   return to ? (
     <Link to={to} css={styles} {...restProps}>
